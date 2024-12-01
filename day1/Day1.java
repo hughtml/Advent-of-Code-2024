1579346e6f93a6443304f8757cde4213da6a0ed4
Hugh <hughmm.fraser@gmail.com>
2024-12-01 21:45:52 +0000

    Create base class and input txt file.

---
+++ day1/Day1.java
@@ -0,0 +1,13 @@
+package day1;
+
+/**
+ * Class to model a solution for Day 1 in Advent of Code 2024
+ * @author Hugh Fraser
+ */
+public class Day1 {
+
+    public static void main(String args) {
+    
+    }
+    
+}
