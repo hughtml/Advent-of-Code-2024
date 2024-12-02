d0b12fb8351424f3043b4e5463a3236ffbf03f70
Hugh <hughmm.fraser@gmail.com>
2024-12-02 18:42:04 +0000

    Complete Day 1.

--- day1/Day1.java
+++ day1/Day1.java
@@ -14,9 +14,13 @@ public class Day1 {
 
     private static ArrayList<Integer> list1 = new ArrayList<>();
     private static ArrayList<Integer> list2 = new ArrayList<>();
+    private static ArrayList<Integer> distanceList = new ArrayList<>();
     public static void main(String[] args) {
         readInput();
         sortLists();
+        findDifferences();
+        System.out.println("The total distance is: " + totalDifference());
+        System.out.println("The similarity score is: " + calculateSimilarityScore());
     }
 
     /**
@@ -47,6 +51,54 @@ public class Day1 {
     public static void sortLists() {
         list1.sort((number1, number2) -> number1.compareTo(number2));
         list2.sort((number1, number2) -> number1.compareTo(number2));
+    }
+
+    /**
+     * Method to find the difference between the distances
+     */
+    public static void findDifferences() {
+        for (int i = 0; i < list1.size(); i++) {
+            distanceList.add(Math.abs(list1.get(i) - list2.get(i)));
+        }
+    }
+
+    /**
+     * Method to find the total of the differences
+     * @return The total added up
+     */
+    public static int totalDifference() {
+        int total = 0;
+        for (Integer number : distanceList) {
+            total += number.intValue();
+        }
+        return total;
+    }
+
+    /**
+     * Method to count the occurrences of a value from one list in another
+     * @param number The value from the first list
+     * @return The amount of times it occurs in the second list
+     */
+    public static int countOccurrences(Integer number) {
+        //The lists have been sorted, so all occurrences will be next to each other
+        int count = list2.lastIndexOf(number) - list2.indexOf(number) + 1;
+        return count;
+    }
+
+    /**
+     * Method to calculate the similarity score between two lists
+     * @return The similarity score
+     */
+    public static int calculateSimilarityScore() {
+        int score = 0;
+
+        for (Integer number : list1) {
+            if (list2.contains(number)) {
+                score += number.intValue() * countOccurrences(number);
+            }
+        }
+
+        return score;
     }
 
 }
