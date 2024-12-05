a9ddb0f72b49db7ba7f1b72a8e3068da86f397ea
Hugh <hughmm.fraser@gmail.com>
2024-12-05 22:28:30 +0000

    Finish Day 3.

--- day3/Day3.java
+++ day3/Day3.java
@@ -18,12 +18,14 @@ public class Day3 {
     private static ArrayList<String> foundInstructions = new ArrayList<>();
     public static void main(String[] args) {
         readInput();
-        System.out.println(corruptedInput);
+        //System.out.println(corruptedInput);
         
         for (String input : corruptedInput) {
             findInstructions(input);
         }
         System.out.println(foundInstructions);
+
+        System.out.println("The total of all the multiplications is: " + runInstructions());
     }
 
     /**
@@ -50,12 +52,43 @@ public class Day3 {
      * @param input The corrupted input to check
      */
     public static void findInstructions(String input) {
-        Pattern pattern = Pattern.compile("mul\\((\\d{1}|\\d{2}|\\d{3}),(\\d{1}|\\d{2}|\\d{3})\\)");
+        Pattern pattern = Pattern.compile("mul\\((\\d{1}|\\d{2}|\\d{3}),(\\d{1}|\\d{2}|\\d{3})\\)|do\\(\\)|don\\'t\\(\\)");
         Matcher matcher = pattern.matcher(input);
 
         while (matcher.find()) {
             foundInstructions.add(input.substring(matcher.start(), matcher.end()));
         }
+    }
+
+    /**
+     * Method to run the instructions found
+     * It splits the instructions to find the values to multiply
+     * @return The total of all the multiplications
+     */
+    public static int runInstructions() {
+        int total = 0;
+        boolean doInstructions = true;
+        for (int i = 0; i < foundInstructions.size(); i++) {
+            if (doInstructions) {
+                System.out.println(foundInstructions.get(i));
+                if (foundInstructions.get(i).equals("don't()")) {
+                    doInstructions = false;
+                } else if (!foundInstructions.get(i).equals("do()")) {
+                    String[] parts = foundInstructions.get(i).split("[\\(\\,\\)]");
+                    total += (Integer.parseInt(parts[1]) * Integer.parseInt(parts[2]));
+                }
+            } else {
+                if (foundInstructions.get(i).equals("do()")) {
+                    doInstructions = true;
+                }
+            }
+            // String output = "";
+            // for (String part : parts) {
+            //     output += part + " : ";
+            // }
+            // System.out.println(output);
+        }
+        return total;
     }
 
 }
