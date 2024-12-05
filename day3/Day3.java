80a4ffe815007e6d414a838828d93709def41ccc
Hugh <hughmm.fraser@gmail.com>
2024-12-05 21:31:50 +0000

    Create method for reading input.

--- day3/Day3.java
+++ day3/Day3.java
@@ -1,9 +1,40 @@
 package day3;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Class to model a solution for Day 3 in Advent of Code 2024
  * @author Hugh Fraser
  */
 public class Day3 {
+
+    private static ArrayList<String> corruptedInput = new ArrayList<>();
+    public static void main(String[] args) {
+        readInput();
+        System.out.println(corruptedInput);
+    }
+
+    /**
+     * Method to read the input from the text file
+     * Takes the .txt file and sorts the two lists into ArrayLists
+     */
+    public static void readInput() {
+
+        File inputFile = new File("day3/Day3Input.txt");
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile))) {
+            String currentLine = bufferedReader.readLine();
+            while (currentLine != null) {
+                corruptedInput.add(currentLine);
+                currentLine = bufferedReader.readLine();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+    }
 
 }
